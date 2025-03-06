3dc3cfb61ad1aba6f5feda56b051fe8c65327e30
muchu1983 <muchu1983@gmail.com>
2025-03-07 01:10:40 +0800

    add instruction fn

--- programs/solana_bury/src/lib.rs
+++ programs/solana_bury/src/lib.rs
@@ -10,7 +10,39 @@ pub mod solana_bury {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn bury(ctx: Context<BuryCtx>) -> Result<()> {
+        // 埋葬指令
+        Ok(())
+    }
+
+    pub fn worship(ctx: Context<WorshipCtx>) -> Result<()> {
+        // 祭拜指令
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct BuryCtx {
+    //埋葬指令所需的參數account
+}
+
+#[derive(Accounts)]
+pub struct WorshipCtx {
+    //祭拜指令所需的參數account
+}
+
+#[account]
+pub struct Tombstone {
+    //墓碑區塊
+    tombstone_id: u8,
+}
+
+#[account]
+pub struct Blessings {
+    //祭拜祝詞區塊
+    tombstone_id: u8,
+}
