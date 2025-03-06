9a752bd65617d28ce1149088c43f055f7c495756
muchu1983 <muchu1983@gmail.com>
2025-03-07 00:11:06 +0800

    add tiny_adventure data

---
+++ app/data/lib.rs
@@ -0,0 +1,90 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("5XnFPRi2S1SwVCnSHhDQ3AA6AY1gq3eHcEd1piU7BWR6");
+
+#[program]
+mod tiny_adventure {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.new_game_data_account.player_position = 0;
+        msg!("A Journey Begins!");
+        msg!("o.......");
+        Ok(())
+    }
+
+    pub fn move_left(ctx: Context<MoveLeft>) -> Result<()> {
+        let game_data_account = &mut ctx.accounts.game_data_account;
+        if game_data_account.player_position == 0 {
+            msg!("You are back at the start.");
+        } else {
+            game_data_account.player_position -= 1;
+            print_player(game_data_account.player_position);
+        }
+        Ok(())
+    }
+
+    pub fn move_right(ctx: Context<MoveRight>) -> Result<()> {
+        let game_data_account = &mut ctx.accounts.game_data_account;
+        if game_data_account.player_position == 3 {
+            msg!("You have reached the end! Super!");
+        } else {
+            game_data_account.player_position = game_data_account.player_position + 1;
+            print_player(game_data_account.player_position);
+        }
+        Ok(())
+    }
+}
+
+fn print_player(player_position: u8) {
+    if player_position == 0 {
+        msg!("A Journey Begins!");
+        msg!("o.......");
+    } else if player_position == 1 {
+        msg!("..o.....");
+    } else if player_position == 2 {
+        msg!("....o...");
+    } else if player_position == 3 {
+        msg!("........\\o/");
+        msg!("You have reached the end! Super!");
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    // We must specify the space in order to initialize an account.
+    // First 8 bytes are default account discriminator,
+    // next 1 byte come from NewAccount.data being type u8.
+    // (u8 = 8 bits unsigned integer = 1 byte)
+    // You can also use the signer as seed [signer.key().as_ref()],
+    #[account(
+        init_if_needed,
+        seeds = [b"level1"],
+        bump,
+        payer = signer,
+        space = 8 + 1
+    )]
+    pub new_game_data_account: Account<'info, GameDataAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MoveLeft<'info> {
+    #[account(mut)]
+    pub game_data_account: Account<'info, GameDataAccount>,
+}
+
+#[derive(Accounts)]
+pub struct MoveRight<'info> {
+    #[account(mut)]
+    pub game_data_account: Account<'info, GameDataAccount>,
+}
+
+#[account]
+pub struct GameDataAccount {
+    player_position: u8,
+}
