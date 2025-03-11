cc5e86452dd379f7f786be97693a709babb132c4
muchu1983 <muchu1983@gmail.com>
2025-03-11 23:47:24 +0800

    move test code to http provider

--- app/http_provider.ts
+++ app/http_provider.ts
@@ -1,13 +1,114 @@
+import * as web3 from "@solana/web3.js"
+import * as anchor from "@coral-xyz/anchor";
+// import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+// import { SolanaBury } from "../target/types/solana_bury";
+// import idl from "../target/idl/solana_bury.json";
 import express from 'express'
 const app = express();
 const port = 8080;
 
-// 定义路由
+// 定義路由
 app.get('/', (req, res) => {
-  res.send('Hello World from TypeScript');
+  res.send('Welcome to solana_bury project.');
 });
 
-// 启动服务器
+app.get('/bury', (req, res) => {
+  res.send('bury some...');
+});
+
+app.get('/worship', (req, res) => {
+  res.send('worship some...');
+});
+
+
+// describe("solana_bury", () => {
+//   // Configure the client to use the local cluster.
+//   // test file
+//   anchor.setProvider(anchor.AnchorProvider.env());
+//   const program = anchor.workspace.SolanaBury as anchor.Program<SolanaBury>;
+
+//   const connection = new web3.Connection("http://127.0.0.1:8899", "confirmed");
+
+//   // http_provider file
+//   // const wallet = useAnchorWallet();
+//   // const provider = new anchor.AnchorProvider(connection, wallet, {});
+//   // anchor.setProvider(provider);
+//   // const program = new anchor.Program(idl as SolanaBury, {
+//   //   connection,
+//   // });
+
+//   const tester_pubkey = new web3.PublicKey("2G1FuUFXviRVr4FX8H8eZtR2WmVBAdFxnUWrxBJDMGvp")
+//   const whose_tombstone = "Bennu"
+
+
+//   it("test bury", async () => {
+//     // 測試埋葬指令
+//     let txHash;
+//     // 尋找墓碑pda位址
+//     const [bennu_tombstone_account] =
+//       web3.PublicKey.findProgramAddressSync([
+//         Buffer.from("solana_bury_tombstone", "utf8"),
+//         Buffer.from("Bennu", "utf8"),
+//         tester_pubkey.toBuffer()
+//         ],
+//         program.programId
+//       );
+//     console.log(bennu_tombstone_account)
+//     // 呼叫埋葬指令
+//     txHash = await program.methods
+//       .bury(whose_tombstone)
+//       .accounts({
+//         tombstone_account: bennu_tombstone_account,
+//         celebrant: tester_pubkey,
+//         systemProgram: web3.SystemProgram.programId,
+//       })
+//       .rpc();
+
+//     // 確認交易成功
+//     await logTransaction(txHash);
+//   });
+
+//   it("test worship", async () => {
+//     // 測試祭拜指令
+//     let txHash;
+//     // 尋找祝詞pda位址
+//     const [bennu_blessings_account] =
+//       web3.PublicKey.findProgramAddressSync([
+//         Buffer.from("solana_bury_blessings", "utf8"),
+//         Buffer.from("Bennu", "utf8"),
+//         tester_pubkey.toBuffer()
+//         ],
+//         program.programId
+//       );
+//     console.log(bennu_blessings_account)
+//     // 呼叫祭拜指令
+//     let offering_count = new anchor.BN(1);
+//     txHash = await program.methods
+//       .worship(whose_tombstone, "R.I.P", offering_count)
+//       .accounts({
+//         blessings_account: bennu_blessings_account,
+//         worshipper: tester_pubkey,
+//         systemProgram: web3.SystemProgram.programId,
+//       })
+//       .rpc();
+
+//     // 確認交易成功
+//     await logTransaction(txHash);
+//   });
+
+//   async function logTransaction(txHash) {
+//     const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+//     await connection.confirmTransaction({
+//       blockhash,
+//       lastValidBlockHeight,
+//       signature: txHash,
+//     });
+//     console.log("solana confirm -v ", txHash)
+//   }
+
+// });
+
+// 啟動http服務
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
--- programs/solana_bury/src/lib.rs
+++ programs/solana_bury/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("4LxT6fYaWmQQyHf7XPcSZnX9AqQpx7Cg6wfjHPDnF8ds");
+declare_id!("4cFbgjxb1aWA729GGsdaBdJQwkeE8Ljd3eK3xRSmP7z9");
 
 #[program]
 pub mod solana_bury {
