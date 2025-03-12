3935eab86e5a4aec42b0c3f1c1ace342b637fb5a
muchu1983 <muchu1983@gmail.com>
2025-03-13 01:13:25 +0800

    redrict / to /index.html

--- app/http_provider.ts
+++ app/http_provider.ts
@@ -6,13 +6,15 @@ const port = 8080;
 
 // 定義路由
 app.get('/', (req, res) => {
-  res.send('\uD840\uDC03\uD840\uDC03\uD840\uDC03Welcome to solana_bury project.𠀃𠀃𠀃');
+  res.redirect(301, '/index.html');
 });
+
 // 測試埋葬指令
 app.get('/bury', async (req, res) => {
   do_bury()
   res.send('bury some...<a href="/index.html">回主頁</a>');
 });
+
 // 測試祭拜指令
 app.get('/worship', async (req, res) => {
   do_worship()
@@ -21,6 +23,7 @@ app.get('/worship', async (req, res) => {
 
 // 使用靜態中間件
 app.use(express.static('app/public')); 
+
 // 啟動http服務
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
