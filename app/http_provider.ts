9c9edcb9b4bca862fa332a32f7689c48926059f3
muchu1983 <muchu1983@gmail.com>
2025-03-12 13:34:38 +0800

    use single quote

--- app/http_provider.ts
+++ app/http_provider.ts
@@ -1,24 +1,23 @@
-import bs58 from 'bs58';
 import BN from 'bn.js';
-import * as web3 from "@solana/web3.js";
-import * as anchor from "@coral-xyz/anchor";
-// import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
-import type { SolanaBury } from "../target/types/solana_bury";
-import idl from "../target/idl/solana_bury.json" with { type: "json" };
+import * as web3 from '@solana/web3.js';
+import * as anchor from '@coral-xyz/anchor';
+// import { useAnchorWallet, useConnection } from '@solana/wallet-adapter-react';
+import type { SolanaBury } from '../target/types/solana_bury';
+import idl from '../target/idl/solana_bury.json' with { type: 'json' };
 import express from 'express';
 const app = express();
 const port = 8080;
 
 // connection
 // const { connection } = useConnection();
-// const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
-const connection = new web3.Connection("http://127.0.0.1:8899", "confirmed");
+// const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
+const connection = new web3.Connection('http://127.0.0.1:8899', 'confirmed');
 
 // wallet
 // const wallet = useAnchorWallet();
 const secret_key_array = [150,142,87,98,48,79,225,72,106,138,197,128,144,90,9,53,109,86,40,93,108,246,234,143,202,129,222,110,122,19,130,23,18,179,176,139,101,175,17,157,47,235,17,111,131,43,174,142,121,89,215,221,101,39,236,13,125,238,244,224,97,12,207,205];
 const key_pair = web3.Keypair.fromSecretKey(Uint8Array.from(secret_key_array));
-// 3. 创建钱包对象
+// 創建錢包
 const wallet = {
   publicKey: key_pair.publicKey,
   signTransaction: (tx: web3.Transaction) => {
@@ -49,8 +48,8 @@ app.get('/bury', async (req, res) => {
   // 尋找墓碑pda位址
   const [bennu_tombstone_account] =
     web3.PublicKey.findProgramAddressSync([
-      Buffer.from("solana_bury_tombstone", "utf8"),
-      Buffer.from("Bennu", "utf8"),
+      Buffer.from('solana_bury_tombstone', 'utf8'),
+      Buffer.from('Bennu', 'utf8'),
       wallet.publicKey.toBuffer()
       ],
       program.programId
@@ -69,7 +68,7 @@ app.get('/bury', async (req, res) => {
 
   // 確認交易成功
   await logTransaction(txHash);
-  res.send("bury some...");
+  res.send('bury some...');
 });
 
 app.get('/worship', async (req, res) => {
@@ -78,8 +77,8 @@ app.get('/worship', async (req, res) => {
   // 尋找祝詞pda位址
   const [bennu_blessings_account] =
     web3.PublicKey.findProgramAddressSync([
-      Buffer.from("solana_bury_blessings", "utf8"),
-      Buffer.from("Bennu", "utf8"),
+      Buffer.from('solana_bury_blessings', 'utf8'),
+      Buffer.from('Bennu', 'utf8'),
       wallet.publicKey.toBuffer()
       ],
       program.programId
@@ -88,7 +87,7 @@ app.get('/worship', async (req, res) => {
   // 呼叫祭拜指令
   let offering_count = new BN(10);
   txHash = await program.methods
-    .worship(whose_tombstone, "R.I.P", offering_count)
+    .worship(whose_tombstone, 'R.I.P', offering_count)
     .accounts({
       blessings_account: bennu_blessings_account,
       worshipper: wallet.publicKey,
@@ -109,7 +108,7 @@ async function logTransaction(txHash) {
     lastValidBlockHeight,
     signature: txHash,
   });
-  console.log("solana confirm -v ", txHash);
+  console.log('solana confirm -v ', txHash);
 }
 
 // 啟動http服務
